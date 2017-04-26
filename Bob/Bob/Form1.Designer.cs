382a19a6b4190bd240dd89365fc1b5bc521f30af
Radar-Games <radargames.jc@gmail.com>
2017-04-26 19:00:10 +0800

    All

---
+++ Bob/Bob/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Bob
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEnable = new System.Windows.Forms.Button();
+            this.btnDisable = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnEnable
+            // 
+            this.btnEnable.Location = new System.Drawing.Point(12, 207);
+            this.btnEnable.Name = "btnEnable";
+            this.btnEnable.Size = new System.Drawing.Size(121, 23);
+            this.btnEnable.TabIndex = 0;
+            this.btnEnable.Text = "Enable Voice Control";
+            this.btnEnable.UseVisualStyleBackColor = true;
+            this.btnEnable.Click += new System.EventHandler(this.btnEnable_Click);
+            // 
+            // btnDisable
+            // 
+            this.btnDisable.Enabled = false;
+            this.btnDisable.Location = new System.Drawing.Point(139, 207);
+            this.btnDisable.Name = "btnDisable";
+            this.btnDisable.Size = new System.Drawing.Size(133, 23);
+            this.btnDisable.TabIndex = 1;
+            this.btnDisable.Text = "Disable Voice Control";
+            this.btnDisable.UseVisualStyleBackColor = true;
+            this.btnDisable.Click += new System.EventHandler(this.btnDisable_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(260, 189);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "- LOG -";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 257);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnDisable);
+            this.Controls.Add(this.btnEnable);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BoB";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEnable;
+        private System.Windows.Forms.Button btnDisable;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Bob/Bob/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using Microsoft.VisualBasic;
+
+namespace Bob
+{
+    public partial class Form1 : Form
+    {
+        // Engines
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        // Personal Variables
+        string userName = "Jamie";
+
+        // Important Shit
+        string time = DateTime.Now.ToString("hh:mm tt");
+
+        // Form 1
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Enable Button Clicked
+        private void btnEnable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            btnDisable.Enabled = true;
+        }
+
+        //Disable Button Clicked
+        private void btnDisable_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsyncStop();
+            btnDisable.Enabled = false;
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Form Load
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Setting Choices and Grammar Builder
+            Choices commands = new Choices();
+            commands.Add(new string[] { "Say hello", "Whats my name", "What are you", "Who is the worst", "Who are you", "What's the time", "Open google"});
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            // IDEK
+            recEngine.LoadGrammarAsync(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+
+            // Misc
+            richTextBox1.Text += "\n";
+        }
+
+        // Functions
+        
+
+        // Speech Recognition
+        private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            switch (e.Result.Text)
+            {
+                // Speech Recognition
+                case "Say hello":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text;
+                    richTextBox1.Text += "\nBoB: Hello " + userName + ". How are you";
+                    richTextBox1.Text += "\n";
+                    synthesizer.SpeakAsync("Hello  " + userName + " How are you?");
+                    break;
+
+                case "Whats my name":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text; 
+                    richTextBox1.Text += "\nBoB: Jamie";
+                    richTextBox1.Text += "\n";
+                    synthesizer.SpeakAsync("Jamie");
+                    break;
+
+                case "What are you":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text;
+                    richTextBox1.Text += "\nBoB: I am a Speech Recognition system programmed in C#";
+                    richTextBox1.Text += "\n";
+                    synthesizer.SpeakAsync("I am a Speech Recognition system programmed in C sharp by Jamie Coulson");
+                    break;
+
+                case "Who is the worst":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text;
+                    richTextBox1.Text += "\nBoB: Scotty Dunc dad";
+                    richTextBox1.Text += "\n";
+                    synthesizer.SpeakAsync("Scotty Dunk Dad");
+                    break;
+
+                case "What's the time":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text;
+                    richTextBox1.Text += "\nBoB: " + time;
+                    richTextBox1.Text += "\n";
+                    synthesizer.SpeakAsync(time);
+                    break;
+
+                case "Open google":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text;
+                    richTextBox1.Text += "\nBoB: Opening Google";
+                    richTextBox1.Text += "\n";
+                    synthesizer.SpeakAsync("Opening Google");
+                    System.Diagnostics.Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+                    break;
+            }
+        }
+    }
+}
