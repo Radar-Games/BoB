225be093d06a601d9af3acb3753fcfac3e206057
Radar-Games <radargames.jc@gmail.com>
2017-04-29 16:37:37 +0800

    sdsd

--- Bob/Bob/Form1.cs
+++ Bob/Bob/Form1.cs
@@ -2,9 +2,38 @@
 using System.Windows.Forms;
 using System.Speech.Recognition;
 using System.Speech.Synthesis;
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
+
 
 namespace Bob
 {
+    public partial class Spotify
+    {
+        private static SpotifyLocalAPI _spotify;
+
+        public static void SPOOF(String[] args)
+        {
+            _spotify = new SpotifyLocalAPI();
+            {
+                SpotifyLocalAPI.RunSpotify();
+            }
+
+            if (!SpotifyLocalAPI.IsSpotifyWebHelperRunning())
+            {
+                SpotifyLocalAPI.RunSpotifyWebHelper();
+            }
+
+            if (!_spotify.Connect())
+                return; //We need to call Connect before fetching infos, this will handle Auth stuff
+
+            StatusResponse status = _spotify.GetStatus(); //status contains infos
+
+            
+        }public static string currentTrack = _spotify.GetStatus().Track.TrackResource.Name;
+    }
+
     public partial class Form1 : Form
     {
         // Engines
@@ -47,7 +76,7 @@ namespace Bob
         {
             // Setting Choices and Grammar Builder
             Choices commands = new Choices();
-            commands.Add(new string[] { "Say hello", "Whats my name", "What are you", "Who is the worst", "Who are you", "What's the time", "Open google", "Will Hellewell", "Why are you so dumb"});
+            commands.Add(new string[] { "Say hello", "Whats my name", "What are you", "Who is the worst", "Who are you", "What's the time", "Open google", "Will Hellewell", "Why are you so dumb", "Say hello to Eloise", "Isn't it bob", "What songs playing"});
             GrammarBuilder gBuilder = new GrammarBuilder();
             gBuilder.Append(commands);
             Grammar grammar = new Grammar(gBuilder);
@@ -60,10 +89,11 @@ namespace Bob
 
             // Misc
             richTextBox1.Text += "\n";
+
+            // Testing
         }
 
         // Functions
-        
 
         // Speech Recognition
         private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
@@ -133,6 +163,27 @@ namespace Bob
                     richTextBox1.Text += "\nBoB: Im only as smart as the person that made me";
                     richTextBox1.Text += "\n";
                     synthesizer.SpeakAsync("Im only as smart as the person that made me");
+                    break;
+
+                case "Say hello to Eloise":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text;
+                    richTextBox1.Text += "\nBoB: Hi Eloise";
+                    richTextBox1.Text += "\n";
+                    synthesizer.SpeakAsync("Hi Eloise");
+                    break;
+
+                case "Isn't it bob":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text;
+                    richTextBox1.Text += "\nBoB: Shut up";
+                    richTextBox1.Text += "\n";
+                    synthesizer.SpeakAsync("Shut up");
+                    break;
+
+                case "What songs playing":
+                    richTextBox1.Text += "\n" + userName + ": " + e.Result.Text;
+                    richTextBox1.Text += "\nBoB: " + Spotify.currentTrack;
+                    richTextBox1.Text += "\n";
+                    //synthesizer.SpeakAsync(_spotify.GetStatus().Track.ToString);
                     break;
             }
         }
